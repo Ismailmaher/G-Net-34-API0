83e5a1ac3fd10933bd0f061f7beb605faa128e03
Isamilmaher <ismailmaher6977@gmail.com>
2026-08-05 21:07:54 +0300

    session5

--- E-Commerce.Application/ApplicationServicesRegesteration.cs
+++ E-Commerce.Application/ApplicationServicesRegesteration.cs
@@ -15,6 +15,7 @@ namespace E_Commerce.Application
         {
             services.AddAutoMapper(typeof(ApplicationServicesRegesteration).Assembly);
             services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IBasketService, BasketService>();
             return services;
         }
     }
---
+++ E-Commerce.Application/Service/BasketService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using E_Commerce.Application.Common;
+using E_Commerce.Application.Contracts;
+using E_Commerce.Application.DTOS.Baskets;
+using E_Commerce.Domain.Contracts;
+using E_Commerce.Domain.Entities.Baskets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Application.Service
+{
+    public class BasketService(IBasketRepository basketRepository, IMapper mapper) : IBasketService
+    {
+        public async Task<Result<BasketDto>> CreateOrUpdateBasketAsync(BasketDto basket, CancellationToken ct = default)
+        {
+            var customerBasket = mapper.Map<CustomerBasket>(basket);
+            var basketresult = await basketRepository.CreateOrUpdateBasketAsync(customerBasket, ct: ct);
+            return basketresult != null ? Result<BasketDto>.Ok(mapper.Map<BasketDto>(basketresult)) : Result<BasketDto>.Fail(Error.Failure("Can Not Create or Update Basket"));
+        }
+
+        public async Task<Result<bool>> DeleteBasketAsync(string basketId, CancellationToken ct = default)
+        {
+           var result = await basketRepository.DeleteBasketAsync(basketId, ct);
+            return result ? Result<bool>.Ok(true) : Result<bool>.Fail(Error.Failure("Can Not Delete Basket"));
+        }
+
+        public async Task<Result<BasketDto>> GetBasketAsync(string Id, CancellationToken ct = default)
+        {
+            var basket = await basketRepository.GetBasketAsync(Id, ct);
+            if (basket == null)
+                return Result<BasketDto>.Fail(Error.Failure("Basket not found"));
+            return mapper.Map<BasketDto>(basket);
+        }
+    }
+}
